2f4b6f8818341e02f1124870115be67a0f523d9b
Duhasky <jeffersoncavpes@gmail.com>
2024-05-23 13:46:02 -0300

    MM-2: table adjusts in theme white

--- resources/views/components/table/tr.blade.php
+++ resources/views/components/table/tr.blade.php
@@ -1,3 +1,3 @@
-<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600">
     {{ $slot }}
 </tr>
