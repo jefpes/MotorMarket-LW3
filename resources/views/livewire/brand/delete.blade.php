86ac86c8347a67f962cf9ee19f446f9765214251
Duhasky <jeffersoncavpes@gmail.com>
2024-05-23 13:31:00 -0300

    MM-2: delete brand

---
+++ resources/views/livewire/brand/delete.blade.php
@@ -0,0 +1,26 @@
+<div>
+  <x-modal wire:model="modal" name="main_modal">
+    <x-slot:title>
+      {{ __($title) }}: <span class="text-yellow-300">{{ $form->name }}</span>
+    </x-slot:title>
+    <div class="w-full">
+      <p>
+        {{__('This operation cannot be undone.')}}
+      </p>
+    </div>
+
+    <x-slot:footer>
+      <x-secondary-button wire:click="cancel">
+        {{ __('Cancel') }}
+      </x-secondary-button>
+
+      <x-danger-button wire:click="destroy" class="ms-3">
+        {{ __('Delete') }}
+      </x-danger-button>
+    </x-slot:footer>
+  </x-modal>
+
+  <x-toast on="show-toast" :$icon>
+    {{ __( $msg ) }}
+  </x-toast>
+</div>
