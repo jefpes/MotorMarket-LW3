a57f08d7b864bbeb6bb5a2f00954ba095ca6e4b9
Duhasky <jeffersoncavpes@gmail.com>
2024-05-23 13:30:46 -0300

    MM-2: create brand

---
+++ resources/views/livewire/brand/create.blade.php
@@ -0,0 +1,24 @@
+<div>
+  <x-primary-button type='button' wire:click="$set('modal', true)" class="text-[1em] tracking-normal">{{ __('New') }}
+      </x-primary-button>
+  <x-modal wire:model="modal" name="main_modal">
+    <x-slot:title> {{ __($title) }} </x-slot:title>
+
+    <x-form.input name="name" label="Name" type="text" placeholder="Name" :messages="$errors->get('form.name')"
+      wire:model="form.name" class="w-full" />
+
+    <x-slot:footer>
+      <x-secondary-button type="button" wire:click="cancel">
+        {{ __('Cancel') }}
+      </x-secondary-button>
+
+     <x-primary-button wire:click="save" class="ms-3">
+        {{ __('Save') }}
+      </x-primary-button>
+    </x-slot:footer>
+  </x-modal>
+
+  <x-toast on="show-toast" :$icon>
+    {{ __( $msg ) }}
+  </x-toast>
+</div>
