d1da5a63f1e10af04e7e6532310188b2ea5364db
Duhasky <jeffersoncavpes@gmail.com>
2024-05-23 17:16:30 -0300

    MM-3: create VehicleModel edit

---
+++ resources/views/livewire/vehicle-model/edit.blade.php
@@ -0,0 +1,27 @@
+<div>
+  <x-modal wire:model="modal" name="edit_role_modal_{{ $form->id }}">
+    <x-slot:title> {{ __('Edit Vehicle Model') }}: <span class="text-yellow-300">{{ $form->name }}</span> </x-slot:title>
+
+    <x-form.input name="name" label="Name" type="text" placeholder="Name" :messages="$errors->get('form.name')"
+      wire:model="form.name" class="w-full" />
+
+    <div class="mt-4">
+      <x-input-label>{{__('Brand')}}</x-input-label>
+      <x-select wire:model="form.brand_id">
+        <option value=""> {{ __('Select a Brand') }} </option>
+        @foreach ($brands as $brand)
+          <option value="{{ $brand->id }}"> {{ $brand->name }} </option>
+        @endforeach
+      </x-select>
+    </div>
+    <x-slot:footer>
+      <x-secondary-button wire:click="$set('modal', false)"> {{ __('Cancel') }} </x-secondary-button>
+
+      <x-primary-button wire:click="save" class="ms-3"> {{ __('Update') }} </x-primary-button>
+    </x-slot:footer>
+  </x-modal>
+
+  <x-toast on="show-toast" :$icon>
+    {{ __( $msg ) }}
+  </x-toast>
+</div>
