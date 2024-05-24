62ff31ab80daa436604d75818b156ed74dfbb4d7
Duhasky <jeffersoncavpes@gmail.com>
2024-05-23 21:31:00 -0300

    MM-4: change theme in login view

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -31,17 +31,23 @@
       </label>
     </div>
 
-    <div class="flex items-center justify-end mt-4">
+    <div class="flex items-center justify-between mt-4">
       @if (Route::has('password.request'))
       <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
         href="{{ route('password.request') }}" wire:navigate>
         {{ __('Forgot your password?') }}
       </a>
       @endif
-
-      <x-primary-button class="ms-3">
-        {{ __('Log in') }}
-      </x-primary-button>
+      <div >
+        <button id="theme-toggle" type="button"
+          class="align-middle text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg px-4">
+          <x-icons.moon id="theme-toggle-dark-icon" />
+          <x-icons.sun id="theme-toggle-light-icon" />
+        </button>
+        <x-primary-button class="ms-3">
+          {{ __('Log in') }}
+        </x-primary-button>
+      </div>
     </div>
   </form>
 </div>
