c06b2e27642330eab910cd93ac7bfb1dcf5fb718
Duhasky <jeffersoncavpes@gmail.com>
2024-05-25 22:12:28 -0300

    MM-5: adjust in css

--- resources/views/livewire/vehicle/index.blade.php
+++ resources/views/livewire/vehicle/index.blade.php
@@ -1,15 +1,15 @@
 <div>
   <x-slot name="header">{{ __($header) }}</x-slot>
-  <div class="flex flex-row flex-wrap md:flex-nowrap w-full mb-4 gap-x-2 gap-y-2">
-    <div class="w-full flex-1">
+  <div class="flex flex-col md:flex-row justify-between gap-2 pb-3">
+    <div class="basis-1/3">
       <x-form.input name="search" type="text" placeholder="Search" :messages="$errors->get('search')"
         wire:model.live.debounce.800="search" class="w-full" />
     </div>
-
-    <div class="gap-2 flex flex-0">
-      <div>
+    <div class="basis-2/3 sm:flex gap-2">
+      <div class="flex-none justify-between">
         <x-text-input type="date" id="date_i" wire:model.live='date_i' /> {{ __('to') }} <x-text-input type="date" id="date_f" wire:model.live='date_f' />
       </div>
+
       <x-select wire:model.live="vehicle_type_id" class="w-full" id="type_select">
         <option value=""> {{ __('Type')}} </option>
         @foreach ($types as $data)
@@ -17,7 +17,7 @@
         @endforeach
       </x-select>
       <x-select wire:model.live="vehicle_model_id" class="w-full" id="model_select">
-        <option value=""> {{ __('Model')}} </option>
+        <option value=""> {{ __('Model') }} </option>
         @foreach ($models as $data)
         <option value="{{ $data->id }}"> {{ $data->name }} </option>
         @endforeach
@@ -26,7 +26,6 @@
         <x-primary-button :href="route('vehicle.create')"  wire:navigate > {{ __('New') }} </x-primary-button>
       @endcan
     </div>
-
   </div>
 
   <div class="flex flex-wrap mx-auto gap-2">
