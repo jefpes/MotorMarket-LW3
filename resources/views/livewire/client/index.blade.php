a0ca6e527570f233f9e4cbb0d76e6769350c7771
Duhasky <jeffersoncavpes@gmail.com>
2024-05-27 16:05:15 -0300

    MM-6: Client Index

---
+++ resources/views/livewire/client/index.blade.php
@@ -0,0 +1,62 @@
+<div>
+  <x-slot name="header">{{ __($header) }}</x-slot>
+  <div class="flex flex-row flex-wrap md:flex-nowrap w-full mb-4 gap-x-2 gap-y-2">
+    <div class="w-full flex-1">
+      <x-form.input name="search" type="text" placeholder="Search" :messages="$errors->get('search')"
+        wire:model.live.debounce.800="search" class="w-full" />
+    </div>
+
+    @can('people_create')
+    <div class="gap-2 flex flex-0">
+      <x-primary-button :href="route('client.create')"  wire:navigate > {{ __('New') }} </x-primary-button>
+    </div>
+    @endcan
+
+  </div>
+
+  <x-table.table>
+    <x-slot:thead>
+      @foreach ($theader as $h)
+        @if ($h == 'Actions')
+          @canany(['client_update', 'client_delete'])
+            <x-table.th> {{ __($h) }} </x-table.th>
+          @endcanany
+          @else
+            <x-table.th> {{ __($h) }} </x-table.th>
+        @endif
+      @endforeach
+    </x-slot:thead>
+    <x-slot:tbody>
+      @foreach ($this->clients->items() as $c)
+      <x-table.tr>
+        <x-table.td> {{ $c->name }} </x-table.td>
+        <x-table.td> {{ $c->rg }} </x-table.td>
+        <x-table.td> {{ $c->cpf }} </x-table.td>
+        <x-table.td> {{ $c->phone_one }} </x-table.td>
+        <x-table.td> {{ $c->birth_date }} </x-table.td>
+
+
+        @canany(['client_delete', 'client_update'])
+        <x-table.td>
+          <div class="flex flex-row gap-2 justify-center">
+
+            @can('client_update')
+              <x-icons.edit class="text-2xl flex text-yellow-400 w-8 h-8 cursor-pointer" id="edit-{{ $c->id }}" href="{{ route('client.edit', $c->id) }}" wire:navigate />
+            @endcan
+
+            @can('client_delete')
+              <x-icons.delete class="cursor-pointer text-2xl flex text-red-600 w-8 h-8" id="btn-delete-{{ $c->id }}" wire:click="$dispatch('client::deleting', { id: {{ $c->id }}})" />
+            @endcan
+          </div>
+        </x-table.td>
+        @endcanany
+      </x-table.tr>
+      @endforeach
+    </x-slot:tbody>
+  </x-table.table>
+  <div class="pt-6 px-2">
+    {{ $this->clients->onEachSide(1)->links() }}
+  </div>
+
+  <livewire:client.delete />
+</div>
