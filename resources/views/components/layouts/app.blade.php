a3f244b8ad7efd68df4046e368d99bc8d63b8a11
Duhasky <jeffersoncavpes@gmail.com>
2024-05-27 13:41:09 -0300

    MM-6: fix toogle theme

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -27,11 +27,13 @@
           <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ $header }}
           </h2>
+          @persist('theme')
           <button id="theme-toggle" type="button"
             class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg px-4">
             <x-icons.moon id="theme-toggle-dark-icon" />
             <x-icons.sun id="theme-toggle-light-icon" />
           </button>
+          @endpersist
         </div>
       </header>
     @endisset
