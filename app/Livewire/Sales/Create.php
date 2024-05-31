8b3f247e069bcbef35f3b8c1825310c1ea237984
Duhasky <jeffersoncavpes@gmail.com>
2024-05-31 14:05:03 -0300

    MM-7: wip

--- app/Livewire/Sales/Create.php
+++ app/Livewire/Sales/Create.php
@@ -2,14 +2,27 @@
 
 namespace App\Livewire\Sales;
 
+use App\Enums\{PaymentMethod, StatusPayments};
+use App\Livewire\Forms\SaleForm;
 use App\Models\{Client, Vehicle};
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Create extends Component
 {
     public ?Vehicle $vehicle;
+
+    public ?SaleForm $form;
+
+    public ?float $originalPrice = 0;
+
+    public ?float $discount = 0;
+
+    public ?float $surchange = 0;
+
+    public ?float $total = 0;
 
     public ?string $header = 'New Sale';
 
@@ -18,15 +31,19 @@ class Create extends Component
         $this->vehicle = Vehicle::query()
             ->with('photos', 'type', 'model')
             ->find($id);
+
+        $this->originalPrice    = $this->vehicle->sale_price;
+        $this->form->vehicle_id = $this->vehicle->id;
+        $this->form->user_id    = auth()->id();
     }
 
     public function render(): View
     {
-        return view('livewire.sales.create');
+        return view('livewire.sales.create', ['payment_method' => PaymentMethod::cases(), 'status' => StatusPayments::cases()]);
     }
 
     #[Computed()]
-    public function clients(): \Illuminate\Support\Collection
+    public function clients(): Collection
     {
         return Client::query()
             ->select('id', 'name')
--- resources/views/livewire/sales/create.blade.php
+++ resources/views/livewire/sales/create.blade.php
@@ -2,5 +2,68 @@
   <x-slot name="header"> {{ __($header) . ': ' . $vehicle->plate . ' - ' . $vehicle->model->name . ' - ' . $vehicle->type->name }} </x-slot>
 
 
+  <div class="space-y-2 mb-4">
+    <div class="flex flex-col md:flex-row justify-between md:space-x-2">
+      <div class="basis-1/3">
+        <x-select wire:model="form.client_id" label="Client" class="w-full">
+          <option value="">{{ __('Select a Client') }}</option>
+          @foreach ($this->clients as $client)
+          <option value="{{ $client->id }}">{{ $client->name }}</option>
+          @endforeach
+        </x-select>
+      </div>
+      <div class="basis-1/3">
+        <x-select wire:model="form.payment_method" label="Payment Method" class="w-full">
+          <option value="">{{ __('Select a Payment Method') }}</option>
+          @foreach ($payment_method as $paymentMethod)
+          <option value="{{ $paymentMethod->value }}">{{ $paymentMethod->value }}</option>
+          @endforeach
+        </x-select>
+      </div>
+      <div class="basis-1/3">
+        <x-select wire:model="form.status" label="Status" class="w-full">
+          <option value="">{{ __('Select a Status') }}</option>
+          @foreach ($status as $s)
+          <option value="{{ $s->value }}">{{ $s->value }}</option>
+          @endforeach
+        </x-select>
+      </div>
+    </div>
+
+    <div class="flex flex-col md:flex-row justify-between md:space-x-2">
+      <div class="md:basis-1/2">
+        <x-form.input type="date" name="date_sale" label="Date Sale" placeholder="Date Sale" :messages="$errors->get('form.date_sale')"
+          wire:model="form.date_sale" class="w-full" />
+      </div>
+      <div class="md:basis-1/2">
+        <x-form.input type="date" name="date_payment" label="Date Payment" placeholder="Date Payment"
+          :messages="$errors->get('form.date_payment')" wire:model="form.date_payment" class="w-full" />
+      </div>
+    </div>
+
+    <div class="flex flex-col md:flex-row justify-between md:space-x-2">
+      <div class="basis-1/2">
+        <x-form.input x-mask="9999999" name="discount" label="Discount" placeholder="Discount"
+          :messages="$errors->get('form.discount')" wire:model="form.discount" class="w-full" />
+      </div>
+      <div class="basis-1/2">
+        <x-form.input x-mask="9999999" name="surchange" label="Surchange" placeholder="Surchange"
+          :messages="$errors->get('form.surchange')" wire:model="form.surchange" class="w-full" />
+      </div>
+    </div>
+
+    <div class="flex flex-col md:flex-row justify-between md:space-x-2">
+      <div class="basis-1/2">
+        <x-form.input disabled name="sale_price" label="Sale Price" placeholder="Sale Price" :messages="$errors->get('form.km')"
+          wire:model="originalPrice" class="w-full" />
+      </div>
+      <div class="basis-1/2">
+        <x-form.input disabled x-mask="99999999999" name="plate" label="Total" placeholder="Total"
+          :messages="$errors->get('total')" wire:model="total" class="w-full" />
+      </div>
+    </div>
+  </div>
+
+
 </div>
 
