3f9028fbdb941b6307bb940b83eb43778f1879ee
Duhasky <jeffersoncavpes@gmail.com>
2024-06-12 14:50:15 -0300

    MM-7: wip

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,7 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Models\{Role, Sale, User, Vehicle, VehicleType};
 use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Dashboard extends Component
@@ -10,5 +13,47 @@ class Dashboard extends Component
     public function render(): View
     {
         return view('livewire.dashboard');
+    }
+
+    #[Computed()]
+    public function users(): int
+    {
+        return User::count();
+    }
+
+    #[Computed()]
+    public function usersNoFunction(): int
+    {
+        return User::whereDoesntHave('roles')->count();
+    }
+
+    #[Computed()]
+    public function roles(): Collection
+    {
+        return Role::withCount('users')->get();
+    }
+
+    #[Computed()]
+    public function stock(): Collection
+    {
+        return Vehicle::with('type')->get();
+    }
+
+    #[Computed()]
+    public function vType(): Collection
+    {
+        return VehicleType::withCount('vehicles')->get();
+    }
+
+    #[Computed()]
+    public function vTypeCount(): Collection
+    {
+        return Vehicle::withCount('type')->get();
+    }
+
+    #[Computed()]
+    public function sales(): Collection
+    {
+        return Sale::with('vehicle')->get();
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,5 +1,62 @@
 <div>
   <x-slot:header> {{ __('Dashboard') }} </x-slot:header>
 
-  {{ __('You are logged in!') }}
+  <div class="flex flex-wrap gap-4">
+    <div class="relative max-w-sm w-full bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+      <span
+        class="bg-blue-200  font-medium text-blue-800 text-center p-0.5 leading-none rounded-full px-2 dark:bg-blue-900 dark:text-blue-200 absolute -translate-y-1/2 translate-x-1/2 right-1/2">
+        {{ __('Users') . ': ' . $this->users }}
+      </span>
+      <ul class="max-w-md space-y-1 list-decimal list-inside text-gray-500 dark:text-gray-400 px-2 py-4">
+        @foreach ($this->roles as $role )
+        <li>
+          {{ __('Role') . ': ' }}<span class="font-semibold text-gray-900 dark:text-white">{{ $role->name }}</span> {{ __('with') }}
+          <span class="font-semibold text-gray-900 dark:text-white">{{ $role->users_count }}</span> {{ __('people') }}
+        </li>
+        @endforeach
+        <li>
+          {{ __('Role') . ': ' }}<span class="font-semibold text-gray-900 dark:text-white">{{ __('No Role') }}</span> {{ __('with') }}
+          <span class="font-semibold text-gray-900 dark:text-white">{{ $this->usersNoFunction }}</span> {{ __('people') }}
+        </li>
+      </ul>
+    </div>
+
+    <div class="relative max-w-sm w-full bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+      <span
+        class="bg-blue-200  font-medium text-blue-800 text-center p-0.5 leading-none rounded-full px-2 dark:bg-blue-900 dark:text-blue-200 absolute -translate-y-1/2 translate-x-1/2 right-1/2">
+        {{ __('Stock') . ': ' . $this->stock->count() }}
+      </span>
+      <ul class="max-w-md space-y-1 list-decimal list-inside text-gray-500 dark:text-gray-400 px-2 py-4">
+        @foreach ($this->vType as $type)
+        <li>
+          {{ __('Type') . ': ' }}<span class="font-semibold text-gray-900 dark:text-white">{{ $type->name }}</span> {{ __('with') }}
+          <span class="font-semibold text-gray-900 dark:text-white">{{ $type->vehicles_count }}</span> {{ __('vehicle') }}
+          <div>
+            {{ __('Total Purchase Price') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$type->vehicles->sum('purchase_price') }}</span> <br/>
+            {{ __('Total Sale Price') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$type->vehicles->sum('sale_price') }}</span>
+          </div>
+        </li>
+        @endforeach
+      </ul>
+    </div>
+
+    <div class="relative max-w-sm w-full bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+      <span
+        class="bg-blue-200  font-medium text-blue-800 text-center p-0.5 leading-none rounded-full px-2 dark:bg-blue-900 dark:text-blue-200 absolute -translate-y-1/2 translate-x-1/2 right-1/2">
+        {{ __('Stock') . ': ' . $this->sales->count() }}
+      </span>
+      <ul class="max-w-md space-y-1 list-decimal list-inside text-gray-500 dark:text-gray-400 px-2 py-4">
+        @foreach ($this->vType as $type)
+        <li>
+          {{ __('Type') . ': ' }}<span class="font-semibold text-gray-900 dark:text-white">{{ $type->name }}</span> {{ __('with') }}
+          <span class="font-semibold text-gray-900 dark:text-white">{{ $type->vehicles_count }}</span> {{ __('vehicle') }}
+          <div>
+            {{ __('Total Purchase Price') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$type->vehicles->sum('purchase_price') }}</span> <br/>
+            {{ __('Total Sale Price') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$type->vehicles->sum('sale_price') }}</span>
+          </div>
+        </li>
+        @endforeach
+      </ul>
+    </div>
+  </div>
 </div>
