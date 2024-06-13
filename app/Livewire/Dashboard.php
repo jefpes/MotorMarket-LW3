d48c8e652e4443d18fcec6ba6e2551c371df3684
Duhasky <jeffersoncavpes@gmail.com>
2024-06-13 10:42:32 -0300

    MM-7: wip

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\{Role, Sale, User, Vehicle, VehicleType};
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
@@ -36,7 +37,7 @@ class Dashboard extends Component
     #[Computed()]
     public function stock(): Collection
     {
-        return Vehicle::with('type')->get();
+        return Vehicle::where('sold_date', null)->with('type')->get();
     }
 
     #[Computed()]
@@ -55,5 +56,22 @@ class Dashboard extends Component
     public function sales(): Collection
     {
         return Sale::with('vehicle')->get();
+    }
+
+    #[Computed()]
+    public function salesType(): Collection
+    {
+        return Sale::join('vehicles', 'sales.vehicle_id', '=', 'vehicles.id')
+                  ->join('vehicle_types', 'vehicles.vehicle_type_id', '=', 'vehicle_types.id')
+                  ->select(
+                      'vehicle_types.name',
+                      DB::raw('COUNT(sales.id) as number_of_sales'),
+                      DB::raw('SUM(sales.total) as total_sales'),
+                      DB::raw('SUM(vehicles.purchase_price) as total_purchase_price'),
+                      DB::raw('SUM(sales.total) - SUM(vehicles.purchase_price) as profit')
+                  )
+                  ->groupBy('vehicle_types.name')
+                  ->get();
+
     }
 }
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -43,16 +43,39 @@
     <div class="relative max-w-sm w-full bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
       <span
         class="bg-blue-200  font-medium text-blue-800 text-center p-0.5 leading-none rounded-full px-2 dark:bg-blue-900 dark:text-blue-200 absolute -translate-y-1/2 translate-x-1/2 right-1/2">
-        {{ __('Stock') . ': ' . $this->sales->count() }}
+        {{ __('Sales') . ': ' . $this->sales->count() }}
       </span>
       <ul class="max-w-md space-y-1 list-decimal list-inside text-gray-500 dark:text-gray-400 px-2 py-4">
-        @foreach ($this->vType as $type)
+        @foreach ($this->salesType as $sale)
         <li>
-          {{ __('Type') . ': ' }}<span class="font-semibold text-gray-900 dark:text-white">{{ $type->name }}</span> {{ __('with') }}
-          <span class="font-semibold text-gray-900 dark:text-white">{{ $type->vehicles_count }}</span> {{ __('vehicle') }}
+          {{ __('Type') . ': ' }}<span class="font-semibold text-gray-900 dark:text-white">{{ $sale->name }}</span> {{ __('with') }}
+          <span class="font-semibold text-gray-900 dark:text-white">{{ $sale->number_of_sales }}</span> {{ __('vehicle') }}
           <div>
-            {{ __('Total Purchase Price') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$type->vehicles->sum('purchase_price') }}</span> <br/>
-            {{ __('Total Sale Price') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$type->vehicles->sum('sale_price') }}</span>
+            {{ __('Total Purchase Price') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$sale->total_purchase_price }}</span> <br/>
+            {{ __('Total Sale Price') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$sale->total_sales }}</span> <br />
+            {{ __('Profit') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$sale->profit }}</span>
+          </div>
+        </li>
+        @endforeach
+      </ul>
+    </div>
+
+    <div class="relative max-w-sm w-full bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+
+
+      <span
+        class="bg-blue-200  font-medium text-blue-800 text-center p-0.5 leading-none rounded-full px-2 dark:bg-blue-900 dark:text-blue-200 absolute -translate-y-1/2 translate-x-1/2 right-1/2">
+        {{ __('Sales') . ': ' . $this->sales->count() }}
+      </span>
+      <ul class="max-w-md space-y-1 list-decimal list-inside text-gray-500 dark:text-gray-400 px-2 py-4">
+        @foreach ($this->salesType as $sale)
+        <li>
+          {{ __('Type') . ': ' }}<span class="font-semibold text-gray-900 dark:text-white">{{ $sale->name }}</span> {{ __('with') }}
+          <span class="font-semibold text-gray-900 dark:text-white">{{ $sale->number_of_sales }}</span> {{ __('vehicle') }}
+          <div>
+            {{ __('Total Purchase Price') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$sale->total_purchase_price }}</span> <br/>
+            {{ __('Total Sale Price') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$sale->total_sales }}</span> <br />
+            {{ __('Profit') . ': ' }} <span class="font-semibold text-gray-900 dark:text-white">{{ ' R$ '.$sale->profit }}</span>
           </div>
         </li>
         @endforeach
