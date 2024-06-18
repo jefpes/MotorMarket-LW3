11a8357a93c75d8226835a8e10e56493c7d2564a
Duhasky <jeffersoncavpes@gmail.com>
2024-06-18 10:50:18 -0300

    MM-16: fix vehicle model create and update

--- resources/views/livewire/vehicle-model/edit.blade.php
+++ resources/views/livewire/vehicle-model/edit.blade.php
@@ -2,30 +2,7 @@
   <x-modal wire:model="modal" name="edit_role_modal_{{ $form->id }}">
     <x-slot:title> {{ __('Edit Vehicle Model') }}: <span class="text-yellow-300">{{ $form->name }}</span> </x-slot:title>
 
-    <x-form.input name="name" label="Name" type="text" placeholder="Name" :messages="$errors->get('form.name')"
-      wire:model="form.name" class="w-full" />
-
-    <div class="mt-4">
-      <x-input-label>{{__('Brand')}}</x-input-label>
-      <x-select wire:model="form.brand_id">
-        <option value=""> {{ __('Select a Brand') }} </option>
-        @foreach ($brands as $brand)
-        <option value="{{ $brand->id }}"> {{ $brand->name }} </option>
-        @endforeach
-      </x-select>
-      <x-input-error :messages="$errors->get('form.brand_id')" />
-    </div>
-
-    <div class="mt-4">
-      <x-input-label>{{__('Type')}}</x-input-label>
-      <x-select wire:model="form.vehicle_type_id">
-        <option value=""> {{ __('Select a Type') }} </option>
-        @foreach ($types as $data)
-        <option value="{{ $data->id }}"> {{ $data->name }} </option>
-        @endforeach
-      </x-select>
-      <x-input-error :messages="$errors->get('form.vehicle_type_id')" />
-    </div>
+    <x-vmodel.create-update />
 
     <x-slot:footer>
       <x-secondary-button wire:click="$set('modal', false)"> {{ __('Cancel') }} </x-secondary-button>
