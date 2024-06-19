1e76942c4eec76102cb2678a635a0c2e4a8b7518
Duhasky <jeffersoncavpes@gmail.com>
2024-06-19 10:44:32 -0300

    MM-20: Seeder

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Company::create([
+            'name'      => 'Motor Market',
+            'cnpj'      => '99.999.999/9999-99',
+            'about'     => 'Somos uma empresa de venda de veículos, na qual presamos pela qualidade e satisfação do cliente.',
+            'phone'     => '(85) 99999-9999',
+            'email'     => 'google.com',
+            'logo'      => 'company-logo.png',
+            'x'         => 'x.com',
+            'instagram' => 'instagram.com',
+            'facebook'  => 'facebook.com',
+            'linkedin'  => 'linkedin.com',
+            'youtube'   => 'youtube.com',
+            'whatsapp'  => 'whatsapp.com',
+        ]);
+
+    }
+}
