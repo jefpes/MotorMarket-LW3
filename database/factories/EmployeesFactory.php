b6a8f9fcca6526ce3b02915b240f887ead11fb4b
Duhasky <jeffersoncavpes@gmail.com>
2024-07-01 14:19:33 -0300

    MM-29: Factory

---
+++ database/factories/EmployeesFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employees>
+ */
+class EmployeesFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'      => $this->faker->name,
+            'email'     => $this->faker->unique()->safeEmail,
+            'phone_one' => $this->faker->phoneNumber,
+            'phone_two' => $this->faker->optional()->phoneNumber,
+            'salary'    => $this->faker->randomFloat(2, 1000, 10000),
+            'rg'        => $this->faker->unique()->numerify('########-#'),
+            'cpf'       => $this->faker->unique()->numerify('###.###.###-##'),
+        ];
+    }
+}
