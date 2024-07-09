c3b971910f0968675cc11efa3910ba0f6cf7d797
Duhasky <jeffersoncavpes@gmail.com>
2024-07-09 15:11:36 -0300

    MM-32-fix-2: wip

--- database/factories/EmployeeAddressFactory.php
+++ database/factories/EmployeeAddressFactory.php
@@ -6,9 +6,9 @@ use App\Enums\States;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EmployeeAddress>
  */
-class ClientAddressFactory extends Factory
+class EmployeeAddressFactory extends Factory
 {
     /**
      * Define the model's default state.
