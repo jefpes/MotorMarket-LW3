dece94bc2d41e525fd65a633e01a6bf44fa1c1d1
Duhasky <jeffersoncavpes@gmail.com>
2024-07-10 22:59:31 -0300

    MM-32-fix-2: toast aprimorated

--- app/Traits/Toast.php
+++ app/Traits/Toast.php
@@ -7,4 +7,18 @@ trait Toast
     public ?string $icon;
 
     public ?string $msg;
+
+    public function toastSuccess(string $msg): void
+    {
+        $this->msg  = $msg;
+        $this->icon = 'icons.success';
+        $this->dispatch('show-toast');
+    }
+
+    public function toastFail(string $msg): void
+    {
+        $this->msg  = $msg;
+        $this->icon = 'icons.fail';
+        $this->dispatch('show-toast');
+    }
 }
