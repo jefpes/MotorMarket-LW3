64790a3951d4e6e1cdc7a983e418f9f2cdc4817b
Duhasky <jeffersoncavpes@gmail.com>
2024-07-11 10:23:38 -0300

    MM-32-fix-2: wip

--- app/Traits/Toast.php
+++ app/Traits/Toast.php
@@ -4,21 +4,24 @@ namespace App\Traits;
 
 trait Toast
 {
-    public ?string $icon;
+    public ?string $icon = null;
 
-    public ?string $msg;
+    public ?string $msg = null;
+
+    protected function toast(string $msg, string $icon): void
+    {
+        $this->msg  = $msg;
+        $this->icon = $icon;
+        $this->dispatch('show-toast');
+    }
 
     public function toastSuccess(string $msg): void
     {
-        $this->msg  = $msg;
-        $this->icon = 'icons.success';
-        $this->dispatch('show-toast');
+        $this->toast($msg, 'icons.success');
     }
 
     public function toastFail(string $msg): void
     {
-        $this->msg  = $msg;
-        $this->icon = 'icons.fail';
-        $this->dispatch('show-toast');
+        $this->toast($msg, 'icons.fail');
     }
 }
