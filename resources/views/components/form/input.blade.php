8f5ce468c494e4911acd5c1ec7d042149925e0a7
Duhasky <jeffersoncavpes@gmail.com>
2024-07-16 13:23:49 -0300

    MM-33: wip

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -8,7 +8,6 @@
   @endif
   <input
     name="{{ $name }}"
-    id="{{ $name }}"
     placeholder="{{ __($placeholder) }}"
     type="{{ $type }}"
     {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600
