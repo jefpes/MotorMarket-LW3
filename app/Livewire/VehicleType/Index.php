b271268dbbc29113d9bd5952bbb600ebd496b0b8
Jefferson <jeffersoncavpes@gmail.com>
2024-07-22 20:14:54 -0300

    MM-33: wip

--- app/Livewire/VehicleType/Index.php
+++ app/Livewire/VehicleType/Index.php
@@ -5,6 +5,7 @@ namespace App\Livewire\VehicleType;
 use App\Enums\Permission;
 use App\Livewire\Forms\VehicleTypeForm;
 use App\Models\{VehicleType};
+use App\Traits\SortTable;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
 use Livewire\Attributes\{Computed, On};
@@ -12,12 +13,29 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    use SortTable;
+
     public VehicleTypeForm $form;
 
     public string $header = 'Vehicle Types';
 
     /** @var array<String> */
     public array $thead = ['Name', 'Actions'];
+
+    /** @return array<object> */
+    #[Computed()]
+    public function table(): array
+    {
+        return [
+            (object)['field' => 'name', 'head' => 'Name'],
+            (object)['field' => 'actions', 'head' => 'Actions'],
+        ];
+    }
+
+    public function mount(): void
+    {
+        $this->setInitialColumn('name');
+    }
 
     #[On('vtype::refresh')]
     public function render(): View
@@ -28,6 +46,6 @@ class Index extends Component
     #[Computed]
     public function data(): Collection
     {
-        return VehicleType::all();
+        return VehicleType::orderBy($this->sortColumn, $this->sortDirection)->get();
     }
 }
--- resources/views/livewire/vehicle-type/index.blade.php
+++ resources/views/livewire/vehicle-type/index.blade.php
@@ -8,13 +8,18 @@
   <div>
     <x-table.table>
       <x-slot:thead>
-        @foreach ($thead as $h)
-          @if ($h == 'Actions')
+        @foreach ($this->table as $h)
+          @if ($h->field == 'actions')
             @canany([$permission::VEHICLE_TYPE_UPDATE->value, $permission::VEHICLE_TYPE_DELETE->value])
-              <x-table.th> {{ __($h) }} </x-table.th>
+              <x-table.th> {{ __($h->head) }} </x-table.th>
             @endcanany
-          @else <x-table.th> {{ __($h) }} </x-table.th> @endif
+          @else
+            <x-table.th class="cursor-pointer" wire:click="doSort('{{ $h->field }}')">
+              <x-table.sortable :columnLabel="$h->head" :columnName='$h->field' :sortColumn="$sortColumn" :sortDirection="$sortDirection" />
+            </x-table.th>
+          @endif
         @endforeach
+
       </x-slot:thead>
       <x-slot:tbody>
         @foreach ($this->data as $d)
