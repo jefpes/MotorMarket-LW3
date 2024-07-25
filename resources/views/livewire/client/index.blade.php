efa262fcb7d9bbf5d0f53a41699716a6bcb91267
Duhasky <jeffersoncavpes@gmail.com>
2024-07-25 10:53:57 -0300

    MM-30: wip

--- resources/views/livewire/client/index.blade.php
+++ resources/views/livewire/client/index.blade.php
@@ -7,7 +7,7 @@
 
     @can($permission_create)
     <div class="gap-2 flex flex-0">
-      <x-primary-button :href="route('supplier.create')"  wire:navigate > {{ __('New') }} </x-primary-button>
+      <x-primary-button :href="route('client.create')"  wire:navigate > {{ __('New') }} </x-primary-button>
     </div>
     @endcan
 
