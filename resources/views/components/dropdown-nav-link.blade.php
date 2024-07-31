80ee4e28d272b60f4aa09a3e909b442e887c4d5d
Jefferson Pessoa <jeffersoncavpes@gmail.com>
2024-07-30 22:19:59 -0300

    MM-37: wip

--- resources/views/components/dropdown-nav-link.blade.php
+++ resources/views/components/dropdown-nav-link.blade.php
@@ -1,37 +1,48 @@
-@props(['active' => false, 'align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white dark:bg-gray-700'])
+
+@props(['active' => false,'align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white dark:bg-gray-700'])
 
 @php
 switch ($align) {
-    case 'left':
-        $alignmentClasses = 'ltr:origin-top-left rtl:origin-top-right start-0';
-        break;
-    case 'top':
-        $alignmentClasses = 'origin-top';
-        break;
-    case 'right':
-    default:
-        $alignmentClasses = 'ltr:origin-top-right rtl:origin-top-left end-0';
-        break;
+case 'left':
+$alignmentClasses = 'ltr:origin-top-left rtl:origin-top-right start-0';
+break;
+case 'top':
+$alignmentClasses = 'origin-top';
+break;
+case 'right':
+default:
+$alignmentClasses = 'ltr:origin-top-right rtl:origin-top-left end-0';
+break;
 }
 
 switch ($width) {
-    case '48':
-        $width = 'w-48';
-        break;
+case '48':
+$width = 'w-48';
+break;
 }
+
+$classes = ($active ?? false)
+? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 dark:border-indigo-600 text-sm font-medium leading-5
+text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+: 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500
+dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700
+focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700
+transition duration-150 ease-in-out';
 @endphp
 
-<div class="relative inline-flex items-center px-1 pt-1 border-b-2 {{ $active ? 'border-indigo-400 dark:border-indigo-600' : 'border-transparent' }} text-sm font-medium leading-5 {{ $active ? 'text-gray-900 dark:text-gray-100' : 'text-gray-500 dark:text-gray-400' }} hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition duration-150 ease-in-out" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
-    <div @click="open = ! open" >
-        {{ $trigger }}
-    </div>
+<div class="relative {{ $classes }}"
+x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+  <div @click="open = ! open">
+    {{ $trigger }}
+  </div>
 
-    <div x-show="open"
-         class="absolute z-50 mt-20 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }} "
-         style="display: none;"
-         @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
-            {{ $content }}
-        </div>
+  <div x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95"
+    x-transition:enter-end="opacity-100 scale-100" x-transition:leave="transition ease-in duration-75"
+    x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95"
+    class="absolute flex-col z-50 mt-32 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}" style="display: none;"
+    @click="open = false">
+    <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
+      {{ $content }}
     </div>
+  </div>
 </div>
--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -13,23 +13,34 @@
         @foreach ($this->navs as $nav)
           @can($nav->permission)
             <div class="hidden sm:-my-px sm:mr-2 sm:flex">
-              <x-nav-link :href="route($nav->route)" :active="$nav->isActive"> {{ __($nav->label) }} </x-nav-link>
+              <x-nav-link :href="route($nav->route)" :active="$nav->isActive" wire:navigate> {{ __($nav->label) }} </x-nav-link>
             </div>
           @endcan
         @endforeach
 
-        <x-dropdown-nav-link :active="request()->routeIs('parent-route.*')" align="right" width="48"
+        <x-dropdown-nav-link :active="request()->routeIs('parent-route.*')"  width="48"
           contentClasses="py-1 bg-white dark:bg-gray-700">
           <x-slot name="trigger">
-            <span>Parent Link</span>
+            <button
+              class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+              <div> Data </div>
+
+              <div class="ms-1">
+                <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
+                  <path fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd" />
+                </svg>
+              </div>
+            </button>
           </x-slot>
           <x-slot name="content">
-            <x-nav-link :href="route('vehicle')" :active="request()->routeIs('vehicle')">
+            <x-dropdown-link :href="route('vehicle')" :active="request()->routeIs('vehicle')">
               Child Link 1
-            </x-nav-link>
-            <x-nav-link :href="route('vehicle')" :active="request()->routeIs('vehicle')">
+            </x-dropdown-link>
+            <x-dropdown-link :href="route('vehicle')" :active="request()->routeIs('vehicle')">
               Child Link 2
-            </x-nav-link>
+            </x-dropdown-link>
             <!-- Add more child links as needed -->
           </x-slot>
         </x-dropdown-nav-link>
