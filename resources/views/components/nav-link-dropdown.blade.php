1d33d01b5a1f62ad0ed2279b51edf0e0bdad7754
Duhasky <jeffersoncavpes@gmail.com>
2024-07-31 10:46:56 -0300

    MM-37: finished

--- resources/views/components/nav-link-dropdown.blade.php
+++ resources/views/components/nav-link-dropdown.blade.php
@@ -1,5 +1,5 @@
 
-@props(['active' => false, 'width' => '48', ])
+@props(['active' => false, 'width' => '48', 'label' ])
 
 @php
 $classes = ($active ?? false)
@@ -14,7 +14,18 @@ transition duration-150 ease-in-out';
 <div class="relative {{ $classes }}"
 x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
   <div @click="open = ! open">
-    {{ $trigger }}
+    <button
+      class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+      <div> {{ __($label) }} </div>
+
+      <div class="ms-1">
+        <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
+          <path fill-rule="evenodd"
+            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+            clip-rule="evenodd" />
+        </svg>
+      </div>
+    </button>
   </div>
 
   <div x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95"
--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -21,22 +21,7 @@
         <!-- Vehicle Dropdown -->
         @canany(array_map(fn($item) => $item->permission, $this->vehicleNavs))
           <div class="hidden sm:-my-px sm:mr-2 sm:flex">
-            <x-nav-link-dropdown :active="request()->routeIs(['vehicle','vehicle.show','vehicle.create','vehicle.edit','vmodel','vtype', 'brand'])"  width="48"
-              contentClasses="py-1 bg-white dark:bg-gray-700">
-              <x-slot name="trigger">
-                <button
-                  class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                  <div> {{ __('Vehicle') }} </div>
-
-                  <div class="ms-1">
-                    <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
-                      <path fill-rule="evenodd"
-                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                        clip-rule="evenodd" />
-                    </svg>
-                  </div>
-                </button>
-              </x-slot>
+            <x-nav-link-dropdown label="Vehicle" :active="request()->routeIs(['vehicle','vehicle.show','vehicle.create','vehicle.edit','vmodel','vtype', 'brand'])"  width="auto">
               <x-slot name="content">
                 @foreach ($this->vehicleNavs as $data)
                   @can($data->permission)
@@ -53,22 +38,7 @@
         <!-- Financial Dropdown -->
         @canany(array_map(fn($item) => $item->permission, $this->financialNavs))
           <div class="hidden sm:-my-px sm:mr-2 sm:flex">
-            <x-nav-link-dropdown :active="request()->routeIs(['sale.create', 'sale.show', 'sales', 'sale.installments', 'installments', 'vehicle-expense'])"  width="48"
-              contentClasses="py-1 bg-white dark:bg-gray-700">
-              <x-slot name="trigger">
-                <button
-                  class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                  <div> {{ __('Financial') }} </div>
-
-                  <div class="ms-1">
-                    <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
-                      <path fill-rule="evenodd"
-                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                        clip-rule="evenodd" />
-                    </svg>
-                  </div>
-                </button>
-              </x-slot>
+            <x-nav-link-dropdown label="Financial" :active="request()->routeIs(['sale.create', 'sale.show', 'sales', 'sale.installments', 'installments', 'vehicle-expense'])"  width="auto">
               <x-slot name="content">
                 @foreach ($this->financialNavs as $data)
                   @can($data->permission)
